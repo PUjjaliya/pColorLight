c3e719aa819d6147752e3cc20f5c07b58864f86c
Prakhar ujjaliya <117422744+PUjjaliya@users.noreply.github.com>
2023-07-27 19:14:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function bulbOnB(){
+    document.getElementById('Dimg').src='blue.png';
+} 
+function bulbOnR(){
+    document.getElementById('Dimg').src='red.png';
+} 
+function bulbOnG(){
+    document.getElementById('Dimg').src='green.png';
+} 
+function bulbOFF(){
+    document.getElementById('Dimg').src='purple.png';
+}
